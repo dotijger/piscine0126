41aa368b588fe17c8d2eae17a4ddc46a6023edee
odin tijger <dotijger@pm.me>
2026-01-29 00:21:11 +0100

    c02 ex02

---
+++ C02/ex02/ft_str_is_alpha.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+int	ft_str_is_alpha(char *str)
+{
+	int	i;
+	i = 0;
+	if (str[i] == '\0')
+		return (1);
+	while (str[i] != '\0')
+	{
+		if (!(str[i] >= 'a' && str[i] <= 'z') || (str[i] >= 'A' && str[i] <= 'Z'))
+		{
+			return (0);
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	main(void)
+{
+	int one;
+	int two;
+	int three;
+
+	char	yes[] = "testo";
+	char	no[] = "test0";
+	char	zero[] = "";
+
+	printf("%d\n", one);
+	printf("%d\n", two);
+	printf("%d\n", three);
+	return 0;
+}
+		
